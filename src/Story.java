878ccea20a886dfee1f2012333304cb6f6c04619
SoulTech-beep <60963125+SoulTech-beep@users.noreply.github.com>
2020-12-13 16:37:15 +0000

    melh0or que antes

--- src/Story.java
+++ src/Story.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionListener;
 public class Story implements ActionListener {
 
 	private UI ui;
-	
+
 	public Story (UI ui) {
 		this.ui=ui;
 	}
@@ -13,16 +13,36 @@ public class Story implements ActionListener {
 	public void actionPerformed(ActionEvent event) {
 		String chosen = event.getActionCommand();
 		switch(ui.position) {
-		case "castleEntrance" :
-			switch (chosen) {
-			case "c1" : break;
-			case "c2" : break;
-			case "c3" : break;
-			}
+			case "castleEntrance" :
+				switch (chosen) {
+					case "c1" : break;
+					case "c2" : break;
+					case "c3" : break;
+				}
 		}
-
 	}
-	
-	
-	
+
+
+	public void changePosition(String position, String text, String ch1, String ch2, String ch3, String background) {
+		ui.textArea.setText(text);
+		ui.position = position;
+		ui.ch1.setText(ch1);
+		ui.ch2.setText(ch2);
+		ui.ch3.setText(ch3);
+		ui.setBackground(background);
+	}
+
+	public void changePosition(String position, String text, String ch1, String ch2, String ch3, String background, String song) {
+		ui.textArea.setText(text);
+		ui.position = position;
+		ui.ch1.setText(ch1);
+		ui.ch2.setText(ch2);
+		ui.ch3.setText(ch3);
+		ui.setBackground(background);
+		ui.setSong(song);
+	}
+
+
+
+
 }
--- src/UI.java
+++ src/UI.java
@@ -3,7 +3,6 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Image;
 import java.awt.Toolkit;
-import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -12,73 +11,77 @@ import javax.swing.WindowConstants;
 public class UI {
 
 	private JFrame frame;
-	private int width;
-	private int height;
-	private Font font = new Font ("Times New Roman", Font.PLAIN, 24);
-
 	protected Image image;
 	protected ImagePanel imagePanel;
+	private int width;
+	private int height;
 	protected Song song;
 	protected String position;
-	protected JTextArea mainTextArea;
+	private Font font = new Font ("Times New Roman", Font.PLAIN, 24);
+	protected JTextArea textArea;
 	protected JButton ch1, ch2, ch3;
 	protected Story story = new Story (this);
 
-
-
 	public void createUI() {
 		//Frame and Background
-		frame = new JFrame ("Castle Adventure");
-		frame.setLayout(null);
-		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		frame.getContentPane().setBackground(Color.BLACK);
-		imagePanel = new ImagePanel();
-		setBackground("backgrounds/b0.jpg");
-		imagePanel.setBounds(0,0, width,height);
-		frame.setSize(width,height);
-		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-		int x = (int) dimension.getWidth()/2 - width/2;
-		int y = (int) dimension.getHeight()/2 - height/2;
-		frame.setLocation(x, y); 
-		frame.setResizable(false);
-		
-		
-		//Story
-		newPosition("castleEntrance","Hello World", "Door on the left","Up the stairs", "Door on the right");
+		frameCreate();
 
-				
+		//Story
+		castleEntrance();
+
 		//Song
-		song = new Song ("songs/s0.wav");
-		song.start();
-		song.loop();
-		
+		setSong("s0.wav");
+
 		frame.setVisible(true);
 	}
-	
+
 	public void setBackground(String name) {
-		ImageIcon img = new ImageIcon(getClass().getClassLoader().getResource(name));
+		ImageIcon img = new ImageIcon(getClass().getClassLoader().getResource("backgrounds/" + name));
 		width = img.getIconWidth();
 		height = img.getIconHeight();
 		image = img.getImage();
 		imagePanel.setImage(image);
 		frame.setContentPane(imagePanel);
 	}
-	
-	private void newPosition(String position, String mainText, String optOneText, String optTwoText, String optThreeText){
-		this.position = position;
+
+	public void setSong(String songLink) {
+		song = new Song ("songs/" + songLink);
+		song.start();
+		song.loop();
+	}
+
+	private void frameCreate() {
+		frame = new JFrame ("Castle Adventure");
+		frame.setLayout(null);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.getContentPane().setBackground(Color.BLACK);
+		imagePanel = new ImagePanel();
+		setBackground("b0.jpg");
+		imagePanel.setBounds(0,0, width,height);
+		frame.setSize(width,height);
+		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+		int x = (int) dimension.getWidth()/2 - width/2;
+		int y = (int) dimension.getHeight()/2 - height/2;
+		frame.setLocation(x, y);
+		frame.setResizable(false);
+	}
+
+	private void castleEntrance() {
+		//Define Player's Position with String
+		position = "castleEntrance";
 
 		//Main Text
-		mainTextArea = new JTextArea(mainText);
-		mainTextArea.setLineWrap(true);
-		mainTextArea.setFont(font);
-		mainTextArea.setBounds(100, height/2+170, 1000, 100);
-		mainTextArea.setBackground(null);
-		mainTextArea.setForeground(Color.BLACK);
-		mainTextArea.setEditable(false);
-		frame.add(mainTextArea);
+		textArea = new JTextArea("Hello World");
+		textArea.setLineWrap(true);
+		textArea.setFont(font);
+		textArea.setBounds(100, height/2+170, 1000, 100);
+		textArea.setBackground(null);
+		textArea.setForeground(Color.BLACK);
+		textArea.setEditable(false);
+		frame.add(textArea);
 
 		//Option One
-		ch1 = new JButton(optOneText);
+		ch1 = new JButton("Door on the left");
 		ch1.setActionCommand("c1");
 		ch1.addActionListener(story);
 		ch1.setBackground(Color.BLACK);
@@ -86,10 +89,9 @@ public class UI {
 		ch1.setFont(font);
 		ch1.setBounds(100, height/2+300, 333, 50);
 		frame.add(ch1);
-		story.actionPerformed(ch1.getAction());
 
 		//Option Two
-		ch2 = new JButton(optTwoText);
+		ch2 = new JButton("Up the stairs");
 		ch2.setActionCommand("c2");
 		ch2.addActionListener(story);
 		ch2.setBackground(Color.BLACK);
@@ -100,7 +102,7 @@ public class UI {
 
 
 		//Option Three
-		ch3 = new JButton(optThreeText);
+		ch3 = new JButton("Door on the right");
 		ch3.setActionCommand("c3");
 		ch3.addActionListener(story);
 		ch3.setBackground(Color.BLACK);
@@ -108,9 +110,7 @@ public class UI {
 		ch3.setFont(font);
 		ch3.setBounds(771, height/2+300, 333, 50);
 		frame.add(ch3);
+
 	}
 
-	public class SomethingHandler implements ActionListener {
-		public void actionPerformed
-	}
 }
