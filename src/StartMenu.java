951eaf677b62e0167a5e336874a2f4bc6005af46
dcgraca <diogocaldeiragraca2000@gmail.com>
2020-12-14 14:36:24 +0000

    Version 1.02 - Diogo

--- src/StartMenu.java
+++ src/StartMenu.java
@@ -13,11 +13,18 @@ public class StartMenu {
     JButton startButton = new JButton();
 
     public StartMenu(){
-        window.setSize(1200,907);
+        window.setLayout(null);
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window.getContentPane().setBackground(Color.BLACK);
-        window.setLayout(null);
 
+        ImageIcon img = new ImageIcon(getClass().getClassLoader().getResource("images/mainMenu.jpg"));
+        Image image = img.getImage();
+        titleImagePanel.setImage(image);
+        window.setContentPane(titleImagePanel);
+        int windowWidth = img.getIconWidth();
+        int windowHeight = img.getIconHeight();
+        titleImagePanel.setBounds(0,0, windowWidth, windowHeight);
+        window.setSize(windowWidth, windowHeight);
 
         Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
         int x = (int) (dimension.getWidth()/2 - 600);
@@ -26,28 +33,6 @@ public class StartMenu {
         window.setResizable(false);
 
         con = window.getContentPane();
-
-        //TODO help já não sei o que fazer aqui!
-//        titleNamePanel.setBounds(100,100,525,375);
-//        titleNamePanel.setImage(new ImageIcon(Objects.requireNonNull(getClass().getClassLoader().getResource("images/title.jpeg"))).getImage());
-//        titleNamePanel.setBackground(Color.BLACK);
-//        titleNameLabel.setText("CASTLE ADVENTURE");
-//        titleNameLabel.setFont(Auxiliary.titleFont);
-//        titleNameLabel.setForeground(Color.white);
-//        titleNamePanel.add(titleNameLabel);
-//        window.setContentPane(titleNamePanel);
-//        con.add(titleNamePanel);
-
-
-        ImageIcon img = new ImageIcon(getClass().getClassLoader().getResource("images/mainMenu.jpeg"));
-        Image image = img.getImage();
-        titleImagePanel.setImage(image);
-        window.setContentPane(titleImagePanel);
-
-
-//
-//        window.add(new JLabel(new ImageIcon("C:\\Users\\Henrique\\IdeaProjects\\AdventureGame\\src\\images\\title.jpg")));
-
 
         startButtonPanel.setBounds(400,400,200,80);
         startButtonPanel.setBackground(Color.RED);
