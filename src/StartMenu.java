f8def3a6d4ec53a211b376c27b47b33ac12d703b
dcgraca <diogocaldeiragraca2000@gmail.com>
2020-12-14 16:16:25 +0000

    Version 1.03 - Diogo

--- src/StartMenu.java
+++ src/StartMenu.java
@@ -7,15 +7,11 @@ import java.awt.event.ActionListener;
 public class StartMenu {
 
     JFrame window = new JFrame("Castle Adventure");
-    Container con;
 
     ImagePanel titleImagePanel = new ImagePanel();
 
-
-    JPanel startButtonPanel = new JPanel();
     JButton startButton = new JButton("START");
 
-    JPanel rulesButtonPanel = new JPanel();
     JButton rulesButton = new JButton("RULES");
 
     public StartMenu(){
@@ -36,15 +32,11 @@ public class StartMenu {
         window.setLocation(x, y);
         window.setResizable(false);
 
-        con = window.getContentPane();
-
-        startButtonPanel.setBounds(400,400,200,80);
-        startButtonPanel.setBackground(Color.RED);
         startButton.setFont(Auxiliary.startBottonText);
         startButton.setBackground(Color.BLACK);
         startButton.setForeground(Color.WHITE);
-        startButtonPanel.add(startButton);
-        con.add(startButtonPanel);
+        startButton.setBounds();
+        window.add(startButton);
 
         Song song = new Song ("songs/mainMenu.wav");
         song.start();
@@ -53,19 +45,17 @@ public class StartMenu {
         startButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                //Stop menu song
                 song.stop();
                 UI ui = new UI();
                 ui.createUI(window);
             }
         });
 
-        rulesButtonPanel.setBounds(400,600,200,80);
-        rulesButtonPanel.setBackground(Color.GREEN);
+
         rulesButton.setFont(Auxiliary.buttonFont);
         rulesButton.setBackground(Color.BLUE);
         rulesButton.setForeground(Color.YELLOW);
-        rulesButtonPanel.add(rulesButton);
-        con.add(rulesButtonPanel);
         rulesButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
