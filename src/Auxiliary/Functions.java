4b917d56c0dd509cf62b660e647585516763119e
dcgraca <diogocaldeiragraca2000@gmail.com>
2020-12-17 19:25:14 +0000

    Version 1.04 - Diogo

--- src/Auxiliary/Functions.java
+++ src/Auxiliary/Functions.java
@@ -7,47 +7,58 @@ import static java.lang.Math.sqrt;
 
 public class Functions {
 
-    public double uniform(){
+
+    //Whether or not an enemy shows up
+    public double uniform() {
         return new Random().nextDouble();
     }
 
+    public int bernoulli( double p ) {
+        assert( 0<=p && p<=1 );
+        int bool =0;
+        if(uniform() < p)
+            bool = 1;
+        return bool;
+    }
+
     //Whether or not an enemy shows up
-    public double binomial (int n, int k, double p){ //n = number of tries; k = number of sucessses; p = probabibility to get it right 1st
-        int answer = 1;
-        for (int i = 1; i <= k; i++) {
-            answer = answer * (n - k + i);
-            answer = answer / i;
-        }
-        return answer * (double)Math.pow(p, k) * (double)Math.pow(1 - p, n - k);
+    public int binomial(int n, int k, double p) { //n = number of tries; k = number of sucessses; p = probabibility to get it right 1st
+        assert ( 0<=p && p<=1 && n>=1);
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+            sum += bernoulli(p);
+        return sum;
     }
 
     //For critical damage (double damage)
-    public double negativeBinomial (int s, double p){
-        assert(s >= 1);
+    public int negativeBinomial( int s, double p ) {
+        assert(s>=1);
         int sum = 0;
-        for (int i = 0; i < s; i++) sum += geometric(p);
+        for ( int i = 0; i < s; i++ )
+            sum += geometric(p);
         return sum;
     }
 
     public int geometric( double p ) {
-        assert( 0. < p && p < 1. );
-        return (int) (log(uniform())/log( 1 - p ));
+        assert( 0 < p && p < 1 );
+        return (int) ( log(uniform()) / log(1-p) );
     }
 
     //The higher the player level, the higher the enemy level is; c=1
-    public double triangular(double xMin, double xMax, double c){
-        assert( xMin < xMax && xMin <= c && c <= xMax );
+    public double triangular( double xMin, double xMax, double c )
+    {
+        assert( xMin<xMax && xMin<=c && c<=xMax );
         double p = uniform();
-        double q = 1. - p;
-        if ( p <= ( c - xMin ) / ( xMax - xMin ) )
-            return xMin + sqrt( ( xMax - xMin ) * ( c - xMin ) * p );
+        double q = 1-p;
+        if ( p <= (c-xMin)/(xMax-xMin) )
+            return xMin + sqrt( (xMax-xMin) * (c-xMin) * p );
         else
-            return xMax - sqrt( ( xMax - xMin ) * ( xMax - c ) * q );
+            return xMax - sqrt( (xMax-xMin) * (xMax-c) * q );
     }
 
     //The higher the player level, the harder it is to run
-    public double exponential( double a, double b ){
-        assert(b > 0);
+    public double exponential( double a, double b ) {
+        assert(b>0);
         return a - b * log(uniform());
     }
 
